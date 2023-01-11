316239eba52b40f1bac9bb4c80f5cc2c082d2d4a
Julian <92728359+Julian1612@users.noreply.github.com>
2023-01-11 23:57:17 +0100

    Added python program

--- shell_cracker.sh
+++ shell_cracker.sh
@@ -6,23 +6,13 @@
 #    By: jschneid <jschneid@student.42heilbronn.    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/01/11 15:58:02 by jschneid          #+#    #+#              #
-#    Updated: 2023/01/11 16:19:45 by jschneid         ###   ########.fr        #
+#    Updated: 2023/01/11 22:36:14 by jschneid         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 x=0
-rm -rf ./output
-mkdir ./output
-while [ $x -le 3 ]
-do
-  printf "Test $x: "
-  file_contents=$(cat cases/case$x.txt)
-  touch ./output/case$x.txt
-  ../minishell < ./cases/case$x.txt > ./output/case$x.txt
-  if diff "output/case$x.txt" "solution/case$x.txt" > /dev/null ; then
-    printf "\033[32m✔\033[0m\n"
-  else
-    printf "\033[31m✘\033[0m\n"
-  fi
-  x=$(( $x + 1 ))
-done
+script_path="test.py"
+
+# mkdir ./output
+python3 $script_path
+# rm -rf ./output
