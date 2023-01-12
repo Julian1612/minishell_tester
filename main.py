91f9a5d880b77e10c0556ffbe18edc40f7c44d4b
Julian <92728359+Julian1612@users.noreply.github.com>
2023-01-12 01:25:29 +0100

    added the comparison function

---
+++ main.py
@@ -0,0 +1,50 @@
+import subprocess
+import os
+import filecmp
+import difflib
+
+def compare_strings(string1, string2):
+	if string1 == string2:
+		return True
+	else:
+		return False
+
+def create_output_file():
+	executable = "./minishell"
+	i = 0
+	while (i < 4):
+		with open(f"./minishell_tester/cases/case{i}.txt", 'r') as c:
+			content = c.read()
+		input_data = content.encode()
+		process = subprocess.Popen([executable], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		output, error = process.communicate(input_data)
+		decode_out = output.decode()
+		print(decode_out)
+		with open(f"./minishell_tester/output/case{i}.txt", "w") as file:
+			print(decode_out, file=file)
+		# filename = f"./minishell_tester/output/case{i}.txt"
+		# with open(filename) as file:
+		# 	lines = file.readlines()
+		# with open(filename, 'w') as file:
+		# 	file.writelines(lines[1:-1])
+		process.wait()
+		process.kill()
+		i += 1
+
+def check_output():
+	i = 0
+	path_output = "./minishell_tester/output"
+	file_count = len(os.listdir(path_output))
+	while(i < file_count):
+		path_solution = f"./minishell_tester/solution/case{i}.txt"
+		path_output_files = f"./minishell_tester/output/case{i}.txt"
+		print(f"case{i + 1}")
+		with open(path_output_files, "r") as f1, open(path_solution, "r") as f2:
+			if compare_strings(f1.read(), f2.read()):
+				print("\033[0;32m✔️\033[0m\n")
+			else:
+				print("❌")
+		i += 1
+
+# create_output_file()
+check_output()
