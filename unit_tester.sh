8ce55de82c29e6e340511dc695310a285a21c64f
Julian <92728359+Julian1612@users.noreply.github.com>
2023-01-31 17:10:48 +0100

    improved bash file

--- unit_tester.sh
+++ unit_tester.sh
@@ -6,16 +6,18 @@
 #    By: jschneid <jschneid@student.42heilbronn.    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/01/11 15:58:02 by jschneid          #+#    #+#              #
-#    Updated: 2023/01/31 14:37:28 by jschneid         ###   ########.fr        #
+#    Updated: 2023/01/31 16:36:55 by jschneid         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/bash
 script_path="main.py"
 
+gsed -i '/\/\/#define RUN_TEST/s/\/\/#define.*/#define RUN_TEST/' ../includes/cub3D.h
 echo "\033[1mCompiling code..."
 cd ..
 make re > /dev/null || exit 1
 cd - > /dev/null
 echo "Running unit tests...\033[0m\n"
 python3 $script_path
+gsed -i '/#define RUN_TEST/s/#define RUN_TEST/\/\/#define RUN_TEST/' ../includes/cub3D.h
